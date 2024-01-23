2ff70e9ed46a834e9773e65c57336516fdfd4238
Stepan Ponomarev <stponomarevw@gmail.com>
2024-01-23 23:04:23 +0300

    fix: comment code that uses protobuf, as it doesn't link

--- benchmark/serialization.cpp
+++ benchmark/serialization.cpp
@@ -1,4 +1,4 @@
-#include "Node.pb.h"
+// #include "Node.pb.h"
 #include "benchmark/benchmark.h"
 #include "storage/FileInfo.hpp"
 #include "storage/FileUtils.hpp"
@@ -20,28 +20,28 @@ static void BM_SerializeInt(benchmark::State &state) {
     }
 }
 
-static void BM_SerializeNode(benchmark::State &state) {
-    std::unique_ptr<storage::FileInfo> file =
-        storage::FileUtils::open_file("./graph_storage-serialization.node", O_RDWR);
-    graph::proto::Node node_a;
-    node_a.set_is_in_use("No");
-    node_a.set_id(0);
-    node_a.set_first_property_id(0);
-    node_a.set_first_relationship_id(0);
-    node_a.set_label("example");
-    node_a.set_remaining_byte("0");
-
-    graph::proto::Node node_b;
-
-    for (auto _ : state) {
-        std::size_t offset_a = virtual_memory::Serializer::serialize_value(node_a, file.get(), 0);
-        std::size_t offset_b = virtual_memory::Serializer::deserialize_value(node_b, file.get(), 0);
-        // node_a.SerializeToFileDescriptor(file->fd);
-        // node_b.ParseFromFileDescriptor(file->fd);
-    }
-}
+// static void BM_SerializeNode(benchmark::State &state) {
+//     std::unique_ptr<storage::FileInfo> file =
+//         storage::FileUtils::open_file("./graph_storage-serialization.node", O_RDWR);
+//     graph::proto::Node node_a;
+//     node_a.set_is_in_use("No");
+//     node_a.set_id(0);
+//     node_a.set_first_property_id(0);
+//     node_a.set_first_relationship_id(0);
+//     node_a.set_label("example");
+//     node_a.set_remaining_byte("0");
+//
+//     graph::proto::Node node_b;
+//
+//     for (auto _ : state) {
+//         std::size_t offset_a = virtual_memory::Serializer::serialize_value(node_a, file.get(), 0);
+//         std::size_t offset_b = virtual_memory::Serializer::deserialize_value(node_b, file.get(), 0);
+//         // node_a.SerializeToFileDescriptor(file->fd);
+//         // node_b.ParseFromFileDescriptor(file->fd);
+//     }
+// }
 
 BENCHMARK(BM_SerializeInt);
-BENCHMARK(BM_SerializeNode);
+// BENCHMARK(BM_SerializeNode);
 
 BENCHMARK_MAIN();
